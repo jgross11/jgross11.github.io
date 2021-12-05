8e0e6c21b2f9d25d91b35db69baa50230904f6ed
jgross11 <jgross11@ycp.edu>
2021-12-05 17:59:31 -0500

    Update notes entry generator.py
    finish MWS of notes generator

--- notes entry generator.py
+++ notes entry generator.py
@@ -1,60 +1,27 @@
 import datetime
-
+import re
 
 def main():
-    '''
-        <h4>Active Projects</h4>
-        <ul>
-            <li>
-                <a href="https://github.com/jgross11/"></a>
-                <ul>
-                    <li>
-                        
-                    </li>
-                    <li>
-                        
-                    </li>
-                </ul>
-            </li>
-            <br>
-            <li>
-                <a href="https://github.com/jgross11/"></a>
-                <ul>
-                    <li></li>
-                </ul>
-            </li>
-        </ul>
-        <h4>Ideas</h4>
-        <ul>
-            <li>
-                
-            </li>
-        </ul>
-        <h4>Personal</h4>
-        <ul>
-            <li>
-                Book: 
-            </li>
-            <li>
-                News:
-            </li>
-        </ul>
-    </div>
-    '''
     date = determine_date()
     header = generate_header(date)
     activities = generate_projects()
+    ideas = generate_ideas()
+    personal = generate_personal()
+    footer = generate_footer()
+    entry = f'''{header}{activities}{ideas}{personal}{footer}'''
+    print(entry)               
 
-    print(header)
+def confirm(query : str) -> bool:
+    answer = input(query + ' y/n: ').lower().strip()
+    return answer == 'y' or answer == 'ye' or answer == 'yes'
 
-def determine_date():
+def determine_date() -> str:
     today = datetime.date.today()
-    # assume entries are created on monday
-    monday = today - datetime.timedelta(days=today.weekday())
-    monday = f'{monday.month}{monday.day}{monday.year}'
-    date_check = input(f"Are you creating an entry for the week of {monday}? y/n: ").lower()
-    if date_check == 'y' or date_check == 'yes':
-        return monday
+    # assume entries are created on sunday
+    sunday = today - datetime.timedelta(days=today.weekday()+1)
+    sunday = f'{sunday.month}{sunday.day}{sunday.year}'
+    if confirm(f"Are you creating an entry for the week of {sunday}?"):
+        return sunday
     else:
         return input("Enter date in format mmddyyyy: ")
 
@@ -62,24 +29,59 @@ def generate_header(date : str) -> str:
     month = date[0:2]
     day = date[2:4]
     year = date[4:]
-    template = f'''
-    <div class="entry">
-        <h3 id="{date}">Week of {month}/{day}/{year}</h3>
-    '''
+    template = f'''<div class="entry"><h3 id="{date}">Week of {month}/{day}/{year}</h3>'''
     return template
     
 def generate_projects() -> str:
-
-    pass
+    text = '<h4>Active Projects</h4><ul>'
+    # projects
+    while True:
+        project_name = input('Enter project name: ')
+        text = text + f'<li><a href="https://github.com/jgross11/">{project_name}</a><ul>'
+        # project entries
+        while True:
+            project_entry = input('Enter task description: ')
+            text = text + f'<li>{project_entry}</li>'
+            if confirm(f'Another entry for {project_name}?'):
+                continue
+            else: 
+                break
+        text = text + '</ul></li>'
+        if confirm('Another project?'):
+            continue
+        else: 
+            break
+    text = text + '</ul>'
+    return text
 
 def generate_ideas() -> str:
-    pass
+    text = '<h4>Ideas</h4><ul>'
+    # ideas
+    while True:
+        idea = input('Enter idea information: ')
+        text = text + f'<li>{idea}</li>'
+        if confirm('Another idea?'):
+            continue
+        else: 
+            break
+    text = text + '</ul>'
+    return text
 
 def generate_personal() -> str:
-    pass
+    text = '<h4>Personal</h4><ul>'
+    # ideas
+    while True:
+        personal = input('Enter personal information: ')
+        text = text + f'<li>{personal}</li>'
+        if confirm('Another personal?'):
+            continue
+        else: 
+            break
+    text = text + '</ul>'
+    return text
 
 def generate_footer() -> str:
-    pass
+    return f'<!-- generated on {datetime.datetime.now()}--></div>'
 
 if __name__ == "__main__":
     main()
